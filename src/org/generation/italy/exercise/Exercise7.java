4627ce7e7a1967f62f59ea333029ae9162b0734c
KetchOxy <manuelsechi330@gmail.com>
2026-05-23 15:10:59 +0200

    svolto esercizio 7

--- src/org/generation/italy/exercise/Exercise7.java
+++ src/org/generation/italy/exercise/Exercise7.java
@@ -8,10 +8,20 @@ public class Exercise7 {
         System.out.println("inserisci un numero: ");
         n = Console.readInt();
         int[][] arrayBD = populateArray(n);
+        for (int j = 0; j < arrayBD.length; j++) {
+            for (int i = 0; i < arrayBD.length; i++) {
+                System.out.printf("%-5s", arrayBD[i][j]);
+            }
+            System.out.println();
+        }
     }
     public static int[][] populateArray(int n){
         int[][] array = new int[n][n];
-
+        for (int i = 0; i< n; i++){
+            for (int j = 0; j < n; j++){
+                array[i][j] =  i == j ? 1 : 0;
+            }
+        }
         return array;
     }
 }
