aea85467382e1b0a0062a6c21ae0a1c755ade843
KetchOxy <manuelsechi330@gmail.com>
2026-05-23 13:32:09 +0200

    iniziato esercizio 7

--- src/org/generation/italy/exercise/Exercise7.java
+++ src/org/generation/italy/exercise/Exercise7.java
@@ -1,4 +1,17 @@
 package org.generation.italy.exercise;
 
+import com.generation.library.Console;
+
 public class Exercise7 {
+    static void main(){
+        int n;
+        System.out.println("inserisci un numero: ");
+        n = Console.readInt();
+        int[][] arrayBD = populateArray(n);
+    }
+    public static int[][] populateArray(int n){
+        int[][] array = new int[n][n];
+
+        return array;
+    }
 }
